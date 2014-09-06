ab34ed2530818034886a84eb5b3ecedf06b53e58
Miha-ha <miha-ha@yandex.ru>
2014-09-06 15:27:21 +0400

    morph garment

--- src/client/js/viewport-ext.js
+++ src/client/js/viewport-ext.js
@@ -19,36 +19,32 @@ ViewportExt.prototype.init = function () {
 
     var $viewport = this.events.$viewport;
 
-//    $(document).tooltip({
-//        track: true,
-//        show: {
-//            delay: 1400
-//        }
-//    });
 
-    this.paramsChanged = {};
-    this.params = {
-        height: {
-            min:145,
-            max:180
-        },
-        chest: {
-            min: 78.68,
-            max: 105.037
-        },
-        underbust: {
-            min: 64.598,
-            max: 86.109
-        },
-        waist: {
-            min: 57.462,
-            max: 96.18
-        },
-        hips: {
-            min: 83.394,
-            max: 124.027
-        }
-    };
+    this.baseParams = this.getBaseParams();
+    /*
+     this.params = {
+     height: {
+     min:145,
+     max:180
+     },
+     chest: {
+     min: 78.68,
+     max: 105.037
+     },
+     underbust: {
+     min: 64.598,
+     max: 86.109
+     },
+     waist: {
+     min: 57.462,
+     max: 96.18
+     },
+     hips: {
+     min: 83.394,
+     max: 124.027
+     }
+     };
+     */
     this.selectedGarment = {};
 
     this.$profile = $viewport.find('#profile');
@@ -68,55 +64,81 @@ ViewportExt.prototype.init = function () {
 
     this.radioGender = new CtrlRadio('#radioGender', this.genderChanged.bind(this));
     this.radioUnits = new CtrlRadio('#radioUnits', this.unitsChanged.bind(this));
+    this.params = {
+        height: new CtrlNumber({
+            selector: '#numberHeight',
+            onChange: this.heightChanged.bind(this),
+            value: this.baseParams.height,
+            min: 145,
+            max: 180
+        }),
+        chest: new CtrlNumber({
+            selector: '#numberChest',
+            onChange: this.chestChanged.bind(this),
+            value: this.baseParams.chest,
+            min: 79,
+            max: 105
+        }),
+        underbust: new CtrlNumber({
+            selector: '#numberUnderbust',
+            onChange: this.underbustChanged.bind(this),
+            value: this.baseParams.underbust,
+            min: 65,
+            max: 86
+        }),
+        waist: new CtrlNumber({
+            selector: '#numberWaist',
+            onChange: this.waistChanged.bind(this),
+            value: this.baseParams.waist,
+            min: 58,
+            max: 96
+        }),
+        hips: new CtrlNumber({
+            selector: '#numberHips',
+            onChange: this.hipsChanged.bind(this),
+            value: this.baseParams.hips,
+            min: 84,
+            max: 124
+        })
+    };
 
-    var body = global.Dressformer.dummy.body;
-    this.numberHeight = new CtrlNumber({
-        selector: '#numberHeight',
-        onChange: this.heightChanged.bind(this),
-        value: body.height.toFixed(0),
-        min: 145,
-        max: 180
-    });
-    this.numberChest = new CtrlNumber({
-        selector: '#numberChest',
-        onChange: this.chestChanged.bind(this),
-        value: body.chest.toFixed(0),
-        min: 79,
-        max: 105
-    });
-    this.numberUnderbust = new CtrlNumber({
-        selector: '#numberUnderbust',
-        onChange: this.underbustChanged.bind(this),
-        value: body.underbust.toFixed(0),
-        min: 65,
-        max: 86
-    });
-    this.numberWaist = new CtrlNumber({
-        selector: '#numberWaist',
-        onChange: this.waistChanged.bind(this),
-        value: body.waist.toFixed(0),
-        min: 58,
-        max: 96
-    });
-    this.numberHips = new CtrlNumber({
-        selector: '#numberHips',
-        onChange: this.hipsChanged.bind(this),
-        value: body.hips.toFixed(0),
-        min: 84,
-        max: 124
-    });
-
-    this.paramsChanged = {}; //reset после установки начальных значений
 };
 
-ViewportExt.prototype.getParams = function () {
-    return [
-        'height='+this.numberHeight.value,
-        'chest='+this.numberChest.value,
-        'underbust='+this.numberUnderbust.value,
-        'waist='+this.numberWaist.value,
-        'hips='+this.numberHips.value
-    ]
+ViewportExt.prototype.getBaseParams = function () {
+    var params = global.Dressformer.dummy.body;
+    for (var param in params) {
+        if (params.hasOwnProperty(param)) {
+            params[param] = +params[param].toFixed(0);
+        }
+    }
+    console.log('base params:', params);
+    return params;
+};
+
+ViewportExt.prototype.getParams = function (onlyChanged) {
+//    return [
+//        'height='+this.numberHeight.value,
+//        'chest='+this.numberChest.value,
+//        'underbust='+this.numberUnderbust.value,
+//        'waist='+this.numberWaist.value,
+//        'hips='+this.numberHips.value
+//    ]
+    var params = [];
+
+//    debugger;
+    for (var param in this.params) {
+        if (this.params.hasOwnProperty(param)) {
+            var value = this.params[param].value;
+            if (onlyChanged && value == this.baseParams[param]) {
+                continue;
+            }
+
+            params.push(param + '=' + value);
+
+        }
+    }
+
+    return params;
 };
 
 ViewportExt.prototype.showProfile = function () {
@@ -139,51 +161,42 @@ ViewportExt.prototype.unitsChanged = function (value) {
 
 ViewportExt.prototype.heightChanged = function (value) {
     console.log('heihgt changed:', value);
-    this.paramsChanged.height = value;
+//    this.paramsChanged.height = value;
 };
 
 ViewportExt.prototype.chestChanged = function (value) {
     console.log('chest changed:', value);
-    this.paramsChanged.chest = value;
+//    this.paramsChanged.chest = value;
 };
 
 ViewportExt.prototype.underbustChanged = function (value) {
     console.log('underbust changed:', value);
-    this.paramsChanged.underbust = value;
+//    this.paramsChanged.underbust = value;
 };
 
 ViewportExt.prototype.waistChanged = function (value) {
     console.log('waist changed:', value);
-    this.paramsChanged.waist = value;
+//    this.paramsChanged.waist = value;
 };
 
 ViewportExt.prototype.hipsChanged = function (value) {
     console.log('hips changed:', value);
-    this.paramsChanged.hips = value;
+//    this.paramsChanged.hips = value;
 };
 
 ViewportExt.prototype.saveProfile = function () {
-    var params = [];
-
-    for (var param in this.paramsChanged) {
-        if (this.paramsChanged.hasOwnProperty(param)){
-            params.push(param+'='+this.paramsChanged[param].toFixed(0))
-        }
-    }
-
-    this.webgl.setParams(params);
+    this.webgl.setParams(this.getParams(true));
 };
 
 ViewportExt.prototype.cancelProfile = function () {
     console.log('cancel profile');
-    var body = global.Dressformer.dummy.body;
-    this.numberHeight.setValue(body.height.toFixed(0));
-    this.numberChest.setValue(body.chest.toFixed(0));
-    this.numberUnderbust.setValue(body.underbust.toFixed(0));
-    this.numberWaist.setValue(body.waist.toFixed(0));
-    this.numberHips.setValue(body.hips.toFixed(0));
 
-    this.paramsChanged = {};
+    for (var param in this.params) {
+        if (this.params.hasOwnProperty(param)) {
+            this.params[param].setValue(this.baseParams[param]);
+        }
+    }
+
 };
 
 ViewportExt.prototype.selectGarment = function (e) {
