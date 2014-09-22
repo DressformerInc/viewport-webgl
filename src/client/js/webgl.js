1da8ced4d51a0ca719ba7b294b803b739c1519e5
Miha-ha <miha-ha@yandex.ru>
2014-09-22 15:04:11 +0400

    fix matcap material

--- src/client/js/webgl.js
+++ src/client/js/webgl.js
@@ -169,7 +169,7 @@ function loadDummy(url, params) {
         shading: THREE.SmoothShading
     });
 
-    //dummyMaterial.uniforms.tMatCap.value.wrapS = dummyMaterial.uniforms.tMatCap.value.wrapT = THREE.ClampToEdgeWrapping;
+    dummyMaterial.uniforms.tMatCap.value.wrapS = dummyMaterial.uniforms.tMatCap.value.wrapT = THREE.ClampToEdgeWrapping;
     url = url || global.Dressformer.user.dummy.assets.geometry.url;
 
     ee.emit('startload');
